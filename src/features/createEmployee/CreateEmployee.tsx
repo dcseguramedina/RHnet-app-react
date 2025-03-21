347259aef53ab3e678c1ceca8746cb2a85f74161
Catalina Segura <catalina.segura@vadesecure.com>
2025-03-21 15:37:44 +0100

    feature: Implement Home page (create employee)

--- src/features/createEmployee/CreateEmployee.tsx
+++ src/features/createEmployee/CreateEmployee.tsx
@@ -3,38 +3,82 @@ import Input from "../../components/input/Input.tsx";
 import Select from "../../components/select/Select";
 import Button from "../../components/button/Button.tsx";
 import styles from "./CreateEmployee.module.css";
+import {departments, states} from "../../data/data.ts";
 
 // Interfaces
-
+interface EmployeeForm {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    startDate: string;
+    street: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    department: string;
+}
 
 // Component creation
 const CreateEmployee: React.FC = () => {
     // React states
-    const [firstName, setFirstName] = useState<string>("");
-    const [lastName, setLastName] = useState<string>("");
-    const [dateOfBirth, setDateOfBirth] = useState<string>("");
-    const [startDate, setStartDate] = useState<string>("");
-    const [street, setStreet] = useState<string>("");
-    const [city, setCity] = useState<string>("");
-    const [state, setState] = useState<string>("");
-    const [zipCode, setZipCode] = useState<string>("");
+    const [employeeForm, setEmployeeForm] = useState<EmployeeForm>({
+        firstName: "",
+        lastName: "",
+        dateOfBirth: "",
+        startDate: "",
+        street: "",
+        city: "",
+        state: "",
+        zipCode: "",
+        department: ""
+    });
 
-    const [selectedValue, setSelectedValue] = useState('');
-
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
+    // Update the employeeForm state
+    // Uses the name attribute of the select element to dynamically update the corresponding property in the state object
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const {name, value} = event.target;
+        setEmployeeForm(prevForm => ({
+            ...prevForm,
+            [name]: value
+        }));
     };
 
-    const options = [
-        { value: 'Sales', label: 'Sales' },
-        { value: 'Marketing', label: 'Marketing' },
-        { value: 'Engineering', label: 'Engineering' },
-        { value: 'Human Resources', label: 'Human Resources' },
-        { value: 'Legal', label: 'Legal' },
-    ];
+    // Update the employeeForm state for input elements
+    // Uses the id attribute of the input element to dynamically update the corresponding property in the state object.
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {id, value} = event.target;
+        setEmployeeForm(prevForm => ({
+            ...prevForm,
+            [id]: value
+        }));
+    };
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        // Get existing employees from localStorage
+        const employees = JSON.parse(localStorage.getItem('employees') || '[]');
+
+        // Add new employee
+        employees.push(employeeForm);
+
+        // Save updated employees array to localStorage
+        localStorage.setItem('employees', JSON.stringify(employees));
+
+        // TO-DO: add modal //
+
+        // Clear form fields and clear errors
+        setEmployeeForm({
+            firstName: "",
+            lastName: "",
+            dateOfBirth: "",
+            startDate: "",
+            street: "",
+            city: "",
+            state: "",
+            zipCode: "",
+            department: ""
+        });
     };
 
     return (
@@ -46,97 +90,86 @@ const CreateEmployee: React.FC = () => {
                         label="First Name"
                         id="firstName"
                         type="text"
-                        value={firstName}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setFirstName(e.target.value)
-                        }
+                        value={employeeForm.firstName}
+                        onChange={handleInputChange}
                         placeholder="First Name"
                         required
+                        aria-required="true"
                     />
                     <Input
                         label="Last Name"
                         id="lastName"
                         type="text"
-                        value={lastName}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setLastName(e.target.value)
-                        }
+                        value={employeeForm.lastName}
+                        onChange={handleInputChange}
                         placeholder="Last Name"
                         required
+                        aria-required="true"
                     />
                     <Input
                         label="Date of Birth"
                         id="dateOfBirth"
                         type="text"
-                        value={dateOfBirth}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setDateOfBirth(e.target.value)
-                        }
+                        value={employeeForm.dateOfBirth}
+                        onChange={handleInputChange}
                         placeholder="Date of Birth"
                         required
+                        aria-required="true"
                     />
                     <Input
                         label="Stard Date"
                         id="startDate"
                         type="text"
-                        value={startDate}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setStartDate(e.target.value)
-                        }
+                        value={employeeForm.startDate}
+                        onChange={handleInputChange}
                         placeholder="Start Date"
                         required
+                        aria-required="true"
                     />
-                    <fieldset className="address">
+                    <fieldset>
                         <legend>Address</legend>
                         <Input
                             label="Street"
                             id="street"
                             type="text"
-                            value={street}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                setStreet(e.target.value)
-                            }
+                            value={employeeForm.street}
+                            onChange={handleInputChange}
                             placeholder="Street"
                             required
+                            aria-required="true"
                         />
                         <Input
                             label="City"
                             id="city"
                             type="text"
-                            value={city}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                setCity(e.target.value)
-                            }
+                            value={employeeForm.city}
+                            onChange={handleInputChange}
                             placeholder="City"
                             required
+                            aria-required="true"
                         />
-                        <Input
+                        <Select
                             label="State"
-                            id="state"
-                            type="text"
-                            value={state}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                setState(e.target.value)
-                            }
-                            placeholder="State"
-                            required
+                            options={states.map(state => ({value: state.abbreviation, label: state.name}))}
+                            value={employeeForm.state}
+                            onChange={handleChange}
+                            name="state"
                         />
                         <Input
                             label="Zip Code"
                             id="zipCode"
                             type="number"
-                            value={zipCode}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                setZipCode(e.target.value)
-                            }
+                            value={employeeForm.zipCode}
+                            onChange={handleInputChange}
                             placeholder="Zip Code"
                             required
+                            aria-required="true"
                         />
                     </fieldset>
                     <Select
                         label="Department"
-                        options={options}
-                        value={selectedValue}
+                        options={departments}
+                        value={employeeForm.department}
                         onChange={handleChange}
                         name="department"
                     />
